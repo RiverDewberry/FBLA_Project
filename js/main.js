24cb58ffb359f5f150043752ce0185d06059edac
River Dewberry <rde80630@oconeeschools.org>
2025-01-11 15:40:52 -0500

    inproved zoom

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { factories } from "./factory.js";
 
-let captureX, captureY, captureW, captureH;
+let captureX, captureY, captureW, captureH, overzoom;
 function factoryAt(x, y) {
     x = Math.round(x * captureW / canvas.width + captureX);
     y = Math.round(y * captureH / canvas.height + captureY);
@@ -10,7 +10,7 @@ function factoryAt(x, y) {
 
     for (let i = 0; i < 8; i++) {
         for (let j = 0; j < 8; j++) {
-            yOff = Math.abs(y - (16 * (i - (7 - j)) + 378));
+            yOff = Math.abs(y - (16 * (i - (7 - j)) + 297));
             xOff = Math.abs(x - (64 * i + 32 * ((7 - j) - i) + 32.5));
             if (xOff + (yOff << 1) < 30.5) return (i << 3) + j;
         }
