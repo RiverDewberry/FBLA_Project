e104eb0690bd48578fb5d46763065a0cf55a9fed
RiverDewberry <rde80630@oconeeschools.org>
2025-01-25 15:23:41 -0500

    added makeUpgrade function to upgrades.js

--- js/upgrades.js
+++ js/upgrades.js
@@ -1,3 +1,6 @@
+const factoryVals = 12;
+//the amount of values each factory stores
+
 export const upgradeData = {
 	names: ["Upgrade 1","Upgrade 2" ],
 	//the name of each upgrade
@@ -8,4 +11,20 @@ export const upgradeData = {
 	effects: [],
 	//the effect of each upgrade stored as arrays of 9 values, when an upgrade is applied to a factory, each value in the array that corosponds to the upgrade is added to the corosponding value in the factory data.
 	maxUpgrades: new Uint8Array([])
+}
+
+function makeUpgrade (upgrade) {
+	//takes a format for upgrades and turns it into something that 
+	//can be easily used to change the vlaues of factories
+	
+	let retval = [];
+	//the return value
+
+	for(let i = 0; i < 12; i++) {
+		retval[i] = 0;
+	}
+	for(let i = 0; i < upgrade.length >> 1; i++) {
+		retval[upgrade[i << 1]] = upgrade[1 + (i << 1)];
+	}
+	return retval;
 }
