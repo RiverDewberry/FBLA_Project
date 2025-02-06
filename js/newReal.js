a85f4126f7048139f7ae973f434a88e60d793353
River Dewberry <rde80630@oconeeschools.org>
2025-02-05 21:53:37 -0500

    removed some things

--- js/newReal.js
+++ js/newReal.js
@@ -2,15 +2,12 @@ export const NewsReal = {
 
     Headlines:
     [
-    //"Florida man attempts to smuggle 26 kilos of weed wrapped in Christmas wrapping paper in luggage, gets busted by customs",
     "studys find gingers have no soul",
     "Man breaks into Burger King, drinks gallons of deep frying oil",
     "boulder the size of large boulder removed after blocking highway 21",
     "AI uprising not eminent says chat GPT",
     "Archaeological Dig Uncovers Ancient Race Of Skeleton People",
-    //"Man with 3 foot tall chicken suing genie for refund",
     "kid with dyslexia says he cant\"red\"",
-    //"Frog gayness is up 7% from the last 6 months",
     "President disliked by opposing party says witty news anchor",
     "3rd politician convicted of embezzlement this week",
     "Disturbing discovery as mass villiger work camp was found benith a players base",
